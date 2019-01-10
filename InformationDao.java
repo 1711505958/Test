799d7cc30036f5797ee60ecdda4eb9f104ffa83f
slebron <1434503368@qq.com>
2019-01-10 11:31:31 +0800

    init again

--- InformationDao.java
+++
@@ -1,9 +0,0 @@
-
-package com.qdu.dao;
-
-import com.qdu.pojo.PersonalInformation;
-
-
-public interface InformationDao extends BaseDao<PersonalInformation>{ 
-    PersonalInformation getInpormationByUid(Integer uid);
-}
